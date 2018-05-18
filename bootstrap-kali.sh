f3bb9c58c576e62edda25c202d650ab2f86fc13b
johnsaigle <john.saigle@gmail.com>
2018-05-18 14:59:23 -0400

    Add passwd. Reorder things. Comments

--- bootstrap-kali.sh
+++ bootstrap-kali.sh
@@ -4,13 +4,9 @@
 # get the latest and greatest
 apt-get update && apt-get upgrade -y && apt-get dist-upgrade -y
 
-# useful 
+# Necessary for installing Guest Additions
 apt-get install linux-headers-$(uname -r) -y
 
-# x86 architecture support
-dpkg --add-architecture i386
-apt-get install lib32z1 lib32ncurses5 lib32stdc++6
-apt-get install libc6-dev-i386
 
 # --> Web
 # php-json library not included by default in Debian/Kali
@@ -18,7 +14,14 @@ apt-get install php-json
 
 
 # --> RE
+
+# ollydbg prereq (useful for Windows re)
 apt-get install wine32 -y
+
+# x86 architecture support
+dpkg --add-architecture i386
+apt-get install lib32z1 lib32ncurses5 lib32stdc++6
+apt-get install libc6-dev-i386
 
 # gdb-peda
 git clone https://github.com/longld/peda.git ~/peda
@@ -26,3 +29,9 @@ echo "source ~/peda/peda.py" >> ~/.gdbinit
 
 # Python pwn tools
 pip install pwntools
+
+# --> User accounts
+
+# root/toor not such a good idea...
+echo "Prompting you to change the root password... (don't use 'toor')
+passwd
